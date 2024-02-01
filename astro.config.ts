5e05b4d26761d54cf6fffba0d2e66dac391715f9
Yiyang Wu <toolmanp@outlook.com>
2024-02-01 17:17:29 +0800

    feat: add site config

--- astro.config.ts
+++ astro.config.ts
@@ -5,7 +5,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://staya.live",
+  site: "https://staysa.live",
   compressHTML: true,
   scopedStyleStrategy: "class",
   image: {
