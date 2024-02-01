cf9681c9136eaac6e2d6391100968e53763914e5
Yiyang Wu <toolmanp@outlook.com>
2024-02-01 17:38:18 +0800

    feat: serve custom domain

--- astro.config.ts
+++ astro.config.ts
@@ -5,8 +5,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://toolmanp.github.io",
-  base: "/STAY_ALIVE",
+  site: "https://staysa.live",
   compressHTML: true,
   scopedStyleStrategy: "class",
   image: {
